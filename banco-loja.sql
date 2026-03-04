f27370e592d248ae2244528a048d250ca32eb679
Luis100000 <luis.dea@escola.pr.gov.br>
2026-03-04 08:30:24 -0300

    Create Produtos table with product details

---
+++ banco-loja.sql
@@ -0,0 +1,10 @@
+1 CREATE TABLE Produtos (
+2 Codigo_Produto INT PRIMARY KEY,
+3
+Nome_Produto VARCHAR(100) NOT NULL,
+4
+Preco_Unitario_Kg DECIMAL (10, 2) NOT NULL,
+5
+Comentarios TEXT
+6);
+7 SELECT * FROM Produtos;
